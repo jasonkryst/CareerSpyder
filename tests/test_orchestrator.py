8e3c9355b55bba1e7a6585ce112137bc83a218bf
Jason Kryst <jasonkryst@gmail.com>
2026-08-13 12:37:20 -0500

    feat: register workday adapter

--- tests/test_orchestrator.py
+++ tests/test_orchestrator.py
@@ -22,6 +22,11 @@ def test_talentbrew_adapter_is_registered():
     assert "talentbrew" in ADAPTERS
 
 
+def test_workday_adapter_is_registered():
+    from app.adapters import ADAPTERS
+    assert "workday" in ADAPTERS
+
+
 def test_run_once_collects_new_jobs_and_isolates_failures(tmp_db_path):
     conn = db.init_db(tmp_db_path)
     good_source = GreenhouseSource(id="s1", name="Good Co", type="greenhouse", board_token="good")
